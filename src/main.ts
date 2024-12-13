ed3c5cf2e74d10225deb452eaa2c21f3e7ba65ad
junyeob kim <junyeob.kim@onch3.co.kr>
2024-12-13 15:02:52 +0900

    feat: validationPipe 속성 추가

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // 불필요 속성은 전달 하지 않는다 default: true
+    forbidNonWhitelisted: true, // 허용되지 않은 속성을 전달 시 에러를 반환한다 default: false
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,11 +1,43 @@
-import { IsNotEmpty, IsOptional } from "class-validator";
+import { IsNotEmpty, IsOptional, registerDecorator, Validate, ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
+
+@ValidatorConstraint()
+class PasswordValidator implements ValidatorConstraintInterface {
+    validate(value: any, validationArguments?: ValidationArguments): Promise<boolean> | boolean {
+        // 비밀번호 길이는 4-8
+        return value.length > 4 && value.length < 8;
+    }
+    defaultMessage(validationArguments?: ValidationArguments): string {
+        return '비밀번호의 길이는 4~8자 여야합니다. 입력된 비밀번호: $value';
+    }
+}
+
+function IsPasswordValid(validationOptions?: ValidationOptions) {
+    return function(object: Object, propertyName: string) {
+        registerDecorator({
+            target: object.constructor,
+            propertyName,
+            options: validationOptions,
+            validator: PasswordValidator,
+        });
+    }
+}
 
 export class UpdateMovieDto {
     @IsNotEmpty()
     @IsOptional()
-    title?: string;
+    title: string;
 
     @IsNotEmpty()
     @IsOptional()
-    genre?: string;
+    genre: string;
+
+
+    // null || undefined
+    // @IsDefined()
+    // @IsNotEmpty()
+    // @IsIn(['a', 'b'])
+    // @IsNotIn(['a', 'b'])
+    // @Validate(PasswordValidator)
+    @IsPasswordValid()
+    test: string;
 }
