f2fc8e6c338183ad128b7316dcf4eabf8d92a05f
junyeob kim <junyeob.kim@onch3.co.kr>
2024-12-26 15:18:20 +0900

    feat: 다이나믹 태스크 스케줄 작업

--- src/common/task.service.ts
+++ src/common/task.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { Cron } from "@nestjs/schedule";
+import { Cron, SchedulerRegistry } from "@nestjs/schedule";
 import { InjectRepository } from "@nestjs/typeorm";
 import { readdir, unlink } from "fs/promises";
 import { join, parse } from "path"; 
@@ -10,7 +10,8 @@ import { Repository } from "typeorm";
 export class taskService {
     constructor(
         @InjectRepository(Movie)
-        private readonly movieRepository: Repository<Movie>
+        private readonly movieRepository: Repository<Movie>,
+        private readonly schedulerRegistry: SchedulerRegistry,
     ){}
 
     logEverySecond(){
@@ -75,5 +76,32 @@ export class taskService {
                 )`
             })
             .execute();
+    }
+
+    // @Cron('* * * * * *', {
+    //     name: 'printer'
+    // })
+    printer(){
+        console.log('print every seconds');
+    }
+
+    // @Cron('*/5 * * * * *')
+    stopper(){
+        console.log('---stopper run---');
+
+        const job = this.schedulerRegistry.getCronJob('printer');
+
+        console.log('# Last Date');
+        console.log(job.lastDate());
+        console.log('# Next Date');
+        console.log(job.nextDate());
+        console.log('# Next Dates');
+        console.log(job.nextDates(5));
+
+        if(job.running){
+            job.stop();
+        }else{
+            job.start();
+        }
     }
 }
