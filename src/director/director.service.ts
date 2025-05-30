b9d90c8cddaae96745920c892731ddabfb21bfde
devtoprod <devtoproduction@gmail.com>
2025-05-30 11:59:33 +0900

    feat: prisma schema director migration

--- src/director/director.service.ts
+++ src/director/director.service.ts
@@ -4,30 +4,42 @@ import { UpdateDirectorDto } from './dto/update-director.dto';
 import { Director } from './entity/director.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { PrismaService } from 'src/common/prisma.sevice';
 
 @Injectable()
 export class DirectorService {
   constructor(
-    @InjectRepository(Director)
-    private readonly directorRepository: Repository<Director>,
+    // @InjectRepository(Director)
+    // private readonly directorRepository: Repository<Director>,
+    private readonly prisma: PrismaService
   ){}
 
   async create(createDirectorDto: CreateDirectorDto) {
-    return await this.directorRepository.save(createDirectorDto);
+    return await this.prisma.director.create({
+      data: {...createDirectorDto}
+    });
+    // return await this.directorRepository.save(createDirectorDto);
   }
 
   async findAll() {
-    return await this.directorRepository.find();
+    return await this.prisma.director.findMany();
+    // return await this.directorRepository.find();
   }
 
   async findOne(id: number) {
-    return await this.directorRepository.findOne({
+    return await this.prisma.director.findUnique({
       where: {id}
     });
+    // return await this.directorRepository.findOne({
+    //   where: {id}
+    // });
   }
 
   async update(id: number, updateDirectorDto: UpdateDirectorDto) {
-    const obj = await this.directorRepository.findOne({
+    // const obj = await this.directorRepository.findOne({
+    //   where: {id}
+    // });
+    const obj = await this.prisma.director.findUnique({
       where: {id}
     });
 
@@ -35,27 +47,40 @@ export class DirectorService {
       throw new NotFoundException("존재하지 않는 ID입니다.");
     }
 
-    await this.directorRepository.update(
-      {id},
-      updateDirectorDto,
-    );
+    // await this.directorRepository.update(
+    //   {id},
+    //   updateDirectorDto,
+    // );
+    await this.prisma.director.update({
+      where: { id },
+      data: { ...updateDirectorDto }
+    });
 
-    const newObj = await this.directorRepository.findOne({
-      where: {id}
+    // const newObj = await this.directorRepository.findOne({
+    //   where: {id}
+    // });
+    const newObj = await this.prisma.director.findUnique({
+      where: { id }
     });
 
     return newObj;
   }
 
   async remove(id: number) {
-    const obj = await this.directorRepository.findOne({
-      where: {id}
+    // const obj = await this.directorRepository.findOne({
+    //   where: {id}
+    // });
+    const obj = await this.prisma.director.findUnique({
+      where: { id }
     });
     if(!obj) {
       throw new NotFoundException("존재하지 않는 ID입니다.");
     }
 
-    await this.directorRepository.delete(id);
+    // await this.directorRepository.delete(id);
+    await this.prisma.director.delete({
+      where: { id }
+    });
 
     return id;
   }
