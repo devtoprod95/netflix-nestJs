c3f6c16ed79579d062878ca826f8dc962e8aa350
devtoprod <devtoproduction@gmail.com>
2025-06-13 16:04:10 +0900

    feat: mongoose 스키마정의2

---
+++ src/movie/schema/movie-detail.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Types } from "mongoose";
+import { Movie } from "./movie.schema";
+
+@Schema({
+    timestamps: true
+})
+export class MovieDetail extends Document {
+    @Prop({
+        required: true
+    })
+    description: string;
+
+    @Prop({
+        type: Types.ObjectId,
+        ref: 'Movie',
+        required: true,
+        unique: true
+    })
+    movie: Movie;
+}
+
+export const MovieDetailSchema = SchemaFactory.createForClass(MovieDetail);
---
+++ src/movie/schema/movie.schema.ts
@@ -0,0 +1,72 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Types } from "mongoose";
+import { User } from "src/user/schema/user.schema";
+import { MovieUserLike } from "./movie-user-like.schema";
+import { MovieDetail } from "./movie-detail.schema";
+
+@Schema({
+    timestamps: true
+})
+export class Movie extends Document {
+    @Prop({
+        type: [{
+            type: Types.ObjectId,
+            ref: 'User'    ,
+            required: true
+        }],
+    })
+    creator: User;
+
+    @Prop({
+        unique: true,
+        required: true,
+    })
+    title: string;
+
+    @Prop({
+        type: [{
+            type: Types.ObjectId,
+            ref: 'Genre'    
+        }],
+    })
+    genres: Genre[];
+
+    @Prop({
+        default: 0
+    })
+    likeCount: number;
+
+    @Prop({
+        default: 0
+    })
+    disLikeCount: number;
+
+    @Prop({
+        type: Types.ObjectId,
+        ref: 'MovieDetail',
+        required: true
+    })
+    detail: MovieDetail;
+
+    @Prop({
+        required: true,
+    })
+    thumbnail: string;
+
+    @Prop({
+        type: Types.ObjectId,
+        ref: 'Director',
+        required: true
+    })
+    director: Director
+
+    @Prop({
+        type: [{
+            type: Types.ObjectId,
+            ref: 'MovieUserLike',
+        }]
+    })
+    likedUsers: MovieUserLike[]
+}
+
+export const MovieSchema = SchemaFactory.createForClass(Movie);
